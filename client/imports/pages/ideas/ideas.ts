39a80e66d53c025764353c0f7cd103fb3c0ea004
Corneezius <cfr2116@columbia.edu>
2017-03-06 20:48:59 -0500

    import dummy ideas as observables

--- client/imports/pages/ideas/ideas.ts
+++ client/imports/pages/ideas/ideas.ts
@@ -1,11 +1,45 @@
 import { Component } from '@angular/core';
+import * as Moment from 'moment';
+import { Observable } from 'rxjs';
 import template from './ideas.html';
 
 @Component({
   template
 })
 export class IdeasPage {
-  constructor() {
+  ideas: Observable<any[]>;
 
+  constructor() {
+    this.ideas = this.findIdeas();
+  }
+
+  private findIdeas(): Observable<any[]> {
+    return Observable.of([
+        {
+        _id: '0',
+        title: 'push a button and a gorille comes',
+        createdAt: Moment().subtract(1, 'hours').toDate()
+        },
+      {
+        _id: '1',
+        title: 'a marketplace for renting babies',
+        createdAt: Moment().subtract(2, 'hours').toDate()
+      },
+      {
+        _id: '2',
+        title: 'send insults to your friends',
+        createdAt: Moment().subtract(1, 'days').toDate()
+      },
+      {
+        _id: '3',
+        title: 'an anthology about faith authored by a million people',
+        createdAt: Moment().subtract(4, 'days').toDate()
+      },
+      {
+        _id: '4',
+        title: 'an app that challenges user to do kind things',
+        createdAt: Moment().subtract(2, 'weeks').toDate()
+        }
+    ]);
   }
 }
