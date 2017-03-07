e41641ba40259761032677be557305998c30bf87
Corneezius <cfr2116@columbia.edu>
2017-03-06 21:47:38 -0500

    fix ionic change styling

--- client/imports/pages/ideas/ideas.ts
+++ client/imports/pages/ideas/ideas.ts
@@ -18,29 +18,54 @@ export class IdeasPage {
     return Observable.of([
         {
         _id: '0',
-        title: 'push a button and a gorille comes',
+        title: 'Push a button and a gorilla comes',
         createdAt: Moment().subtract(1, 'hours').toDate()
         },
       {
         _id: '1',
-        title: 'a marketplace for renting babies',
+        title: 'A marketplace for renting babies',
         createdAt: Moment().subtract(2, 'hours').toDate()
       },
       {
         _id: '2',
-        title: 'send insults to your friends',
+        title: 'Aend insults to your friends',
         createdAt: Moment().subtract(1, 'days').toDate()
       },
       {
         _id: '3',
-        title: 'an anthology about faith authored by a million people',
+        title: 'An anthology about faith authored by a million people',
         createdAt: Moment().subtract(4, 'days').toDate()
       },
       {
         _id: '4',
-        title: 'an app that challenges user to do kind things',
+        title: 'An app that challenges user to do kind things',
         createdAt: Moment().subtract(2, 'weeks').toDate()
-        }
+      },
+      {
+      _id: '5',
+      title: 'A story about a boy who brings his pet rocks to life',
+      createdAt: Moment().subtract(1, 'hours').toDate()
+      },
+    {
+      _id: '6',
+      title: '1 billion people singing kumbaya on new years eve 2018',
+      createdAt: Moment().subtract(2, 'hours').toDate()
+    },
+    {
+      _id: '7',
+      title: 'A game where you give a kid neopet and then kidnap it to help kid learn',
+      createdAt: Moment().subtract(1, 'days').toDate()
+    },
+    {
+      _id: '8',
+      title: 'Tinder consulting',
+      createdAt: Moment().subtract(4, 'days').toDate()
+    },
+    {
+      _id: '9',
+      title: 'An app that challenges user to do kind things',
+      createdAt: Moment().subtract(2, 'weeks').toDate()
+      }
     ]);
   }
 }
