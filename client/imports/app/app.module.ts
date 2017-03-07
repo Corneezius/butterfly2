872bc02db3b098cccbe14ff2ab7721b0922ae189
Corneezius <cfr2116@columbia.edu>
2017-03-06 20:42:04 -0500

    add ideas page to app module

--- client/imports/app/app.module.ts
+++ client/imports/app/app.module.ts
@@ -1,17 +1,20 @@
 import { NgModule, ErrorHandler } from '@angular/core';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
+import { IdeasPage } from '../pages/ideas/ideas';
 import { MyApp } from './app.component';
 
 @NgModule({
   declarations: [
-    MyApp
+    MyApp,
+    IdeasPage
   ],
   imports: [
     IonicModule.forRoot(MyApp),
   ],
   bootstrap: [IonicApp],
   entryComponents: [
-    MyApp
+    MyApp,
+    IdeasPage
   ],
   providers: [
   { provide: ErrorHandler, useClass: IonicErrorHandler }
