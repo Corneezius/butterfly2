a8b527c444fc86e330d7470c3809a59247d3b099
Corneezius <cfr2116@columbia.edu>
2017-03-12 01:14:17 -0500

    successfully preload fixtures to server and then pull data from server to client. also implement remove idea function

--- client/imports/pages/ideas/ideas.ts
+++ client/imports/pages/ideas/ideas.ts
@@ -1,71 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import * as Moment from 'moment';
 import { Observable } from 'rxjs';
-import { Idea } from '../../../../imports/models';
+import { Ideas} from '../../../../imports/collections';
+import { Idea} from '../../../../imports/models';
 import template from './ideas.html';
 
 @Component({
   template
 })
-export class IdeasPage {
-  ideas: Observable<Idea[]>;
+export class IdeasPage implements OnInit {
+  ideas;
 
   constructor() {
-    this.ideas = this.findIdeas();
   }
 
-  private findIdeas(): Observable<Idea[]> {
-    return Observable.of([
-        {
-        _id: '0',
-        title: 'Push a button and a gorilla comes',
-        createdAt: Moment().subtract(1, 'hours').toDate();
-        },
-      {
-        _id: '1',
-        title: 'A marketplace for renting babies',
-        createdAt: Moment().subtract(2, 'hours').toDate()
-      },
-      {
-        _id: '2',
-        title: 'Aend insults to your friends',
-        createdAt: Moment().subtract(1, 'days').toDate()
-      },
-      {
-        _id: '3',
-        title: 'An anthology about faith authored by a million people',
-        createdAt: Moment().subtract(4, 'days').toDate()
-      },
-      {
-        _id: '4',
-        title: 'An app that challenges user to do kind things',
-        createdAt: Moment().subtract(2, 'weeks').toDate()
-      },
-      {
-      _id: '5',
-      title: 'A story about a boy who brings his pet rocks to life',
-      createdAt: Moment().subtract(1, 'hours').toDate()
-      },
-    {
-      _id: '6',
-      title: '1 billion people singing kumbaya on new years eve 2018',
-      createdAt: Moment().subtract(2, 'hours').toDate()
-    },
-    {
-      _id: '7',
-      title: 'A game where you give a kid neopet and then kidnap it to help kid learn',
-      createdAt: Moment().subtract(1, 'days').toDate()
-    },
-    {
-      _id: '8',
-      title: 'Tinder consulting',
-      createdAt: Moment().subtract(4, 'days').toDate()
-    },
-    {
-      _id: '9',
-      title: 'An app that challenges user to do kind things',
-      createdAt: Moment().subtract(2, 'weeks').toDate()
-      }
-    ]);
+  ngOnInit() {
+    this.ideas = Ideas
+      .find({})
+      .zone();
   }
-}
+
+  removeIdea(idea: Idea): void {
+     Ideas.remove({_id: idea._id}).subscribe(() => {});
+   }
+ };
